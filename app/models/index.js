6782a2b1ab2911d986a252cd192339e7e83d6ed7
Hagen DeRouen <hagend87@gmail.com>
2021-04-27 14:50:43 -0500

    Add starting models and associations.

---
+++ app/models/index.js
@@ -0,0 +1,22 @@
+const Post = require('./Post');
+const Comment = require('./Comment');
+const User = require('./User');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
+
